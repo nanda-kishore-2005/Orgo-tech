5de548a322e6c1b4046a1fb114eb8c412ebeb209
Manne Nanda Kishore <2300032049@kluniversity.in>
2024-09-13 15:00:28 +0530

    Update v4

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }  
+    cookie: { secure: true }  
 }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
